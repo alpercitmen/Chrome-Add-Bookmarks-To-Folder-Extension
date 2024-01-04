14c246ddce8d91a7d24b81db561d31d02454f7ab
Alper Çitmen <alper.citmen@gmail.com>
2024-01-04 11:48:51 +0300

    Refactor chrome.tabs.onUpdated.addListener in background.js

--- background.js
+++ background.js
@@ -12,13 +12,17 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
     });
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    var tabUrl = tab.url;
-    chrome.bookmarks.search({ url: tabUrl }, function (bookmarks) {
-        if (bookmarks.length > 0) {
-            chrome.action.setIcon({ path: 'icons/icon_10.png' });
-        } else {
-            chrome.action.setIcon({ path: 'icons/icon_9.png' });
-        }
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function (tab) {
+        chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+            var tabUrl = tab.url;
+            chrome.bookmarks.search({ url: tabUrl }, function (bookmarks) {
+                if (bookmarks.length > 0) {
+                    chrome.action.setIcon({ path: 'icons/icon_10.png' });
+                } else {
+                    chrome.action.setIcon({ path: 'icons/icon_9.png' });
+                }
+            });
+        });
     });
 });
